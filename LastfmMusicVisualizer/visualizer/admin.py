13eb0160f43396a7a1bafa76c888e4c91a3bd692
Samuel Heinrich <shazamuel89@gmail.com>
2025-11-24 21:55:40 -0600

    Set up database models, migrations, and admin interface for user profiles, Last.fm data, and visualizations; update URL routing, add views, and configure 'visualizer' app in project settings

--- LastfmMusicVisualizer/visualizer/admin.py
+++ LastfmMusicVisualizer/visualizer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SiteUserProfile, LastfmUserProfile, Visualization
+
+admin.site.register(SiteUserProfile)
+admin.site.register(LastfmUserProfile)
+admin.site.register(Visualization)
--- LastfmMusicVisualizer/visualizer/urls.py
+++ LastfmMusicVisualizer/visualizer/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('stats/loading/<str:username>/', views.loading_user_stats, name='loading_user_stats'),
+    path('stats/<str:username>/', views.user_stats, name='user_stats'),
+
+    path('visualization/options/<str:username>/', views.visualization_options, name='visualization_options'),
+    path('visualization/loading/<int:visualization_id>/', views.loading_visualization, name='loading_visualization'),
+    path('visualization/result/<int:visualization_id>/', views.visualization_result, name='visualization_result'),
+
+    path('account/', views.account, name='account'),
+
+    path('about/', views.about, name='about')
 ]
